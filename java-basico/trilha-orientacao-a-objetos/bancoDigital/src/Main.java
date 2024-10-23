5b40c24cc7e823fe94540e7bb907a173476c50bd
gildeanM <gildean.m098@gmail.com>
2024-10-23 16:02:04 -0300

    implementation of the code of lab-banco-digital

---
+++ java-basico/trilha-orientacao-a-objetos/bancoDigital/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+    }
+}
