a9941739ed3afce71ee0a5793e11addd5a61b216
gildeanM <gildean.m098@gmail.com>
2024-10-25 15:36:51 -0300

    finished the challenge

--- trilha-orientacao-a-objetos/abstracao-bootcamp/src/br/com/dio/desafio/dominio/Mentoria.java
+++ trilha-orientacao-a-objetos/abstracao-bootcamp/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,35 +2,24 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private LocalDate data;
+
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
 
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -40,4 +29,6 @@ public class Mentoria {
     public void setData(LocalDate data) {
         this.data = data;
     }
+
+
 }
