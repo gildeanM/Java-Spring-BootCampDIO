6996b8974a9d154ef66ce7b927bca5d844cc6ae7
gildeanM <gildean.m098@gmail.com>
2024-11-04 14:54:32 -0300

    initializing with data spring jpa

---
+++ trilha-spring-framework/spring-data-jpa/src/main/java/dio/spring_data_jpa/StartApp.java
@@ -0,0 +1,30 @@
+package dio.spring_data_jpa;
+
+
+import dio.spring_data_jpa.model.User;
+import dio.spring_data_jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("Gildean");
+        user.setUsername("gilms");
+        user.setPassword("1234569");
+
+        repository.save(user);
+
+        for (User u: repository.findAll()){
+            System.out.println(u);
+        }
+
+    }
+}
