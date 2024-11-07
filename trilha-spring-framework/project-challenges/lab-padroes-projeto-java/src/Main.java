cf9e859d7e4d369e7942219f60010bae6b602c0d
gildeanM <gildean.m098@gmail.com>
2024-11-07 14:48:58 -0300

    practicing with pure java: strategy

--- trilha-spring-framework/project-challenges/lab-padroes-projeto-java/src/Main.java
+++ trilha-spring-framework/project-challenges/lab-padroes-projeto-java/src/Main.java
@@ -1,38 +1,56 @@
-import one.digitalinnovation.gof.SingletonEager;
-import one.digitalinnovation.gof.SingletonLazy;
-import one.digitalinnovation.gof.SingletonLazyHolder;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Utilizando Singleton Lazy");
-        SingletonLazy lazy = SingletonLazy.getInstance();
+//        System.out.println("Utilizando Singleton Lazy");
+//        SingletonLazy lazy = SingletonLazy.getInstance();
+//
+//        System.out.println(lazy);
+//
+//        lazy = SingletonLazy.getInstance();
+//
+//        System.out.println(lazy);
+//
+//        System.out.println("Utilizando Singleton Eager");
+//        SingletonEager eager = SingletonEager.getInstance();
+//
+//        System.out.println(eager);
+//
+//        eager = SingletonEager.getInstance();
+//
+//        System.out.println(eager);
+//
+//        System.out.println("Utilizando Singleton Lazy Holder");
+//        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+//
+//        System.out.println(lazyHolder);
+//
+//        lazyHolder = SingletonLazyHolder.getInstance();
+//
+//        System.out.println(lazyHolder);
 
-        System.out.println(lazy);
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+        Comportamento defensivo = new ComportamentoDefensivo();
 
-        lazy = SingletonLazy.getInstance();
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
 
-        System.out.println(lazy);
+        robo.setComportamento(defensivo);
+        robo.mover();
 
-        System.out.println("Utilizando Singleton Eager");
-        SingletonEager eager = SingletonEager.getInstance();
+        robo.setComportamento(agressivo);
+        robo.mover();
 
-        System.out.println(eager);
 
-        eager = SingletonEager.getInstance();
 
-        System.out.println(eager);
-
-        System.out.println("Utilizando Singleton Lazy Holder");
-        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
-
-        System.out.println(lazyHolder);
-
-        lazyHolder = SingletonLazyHolder.getInstance();
-
-        System.out.println(lazyHolder);
 
 
     }
